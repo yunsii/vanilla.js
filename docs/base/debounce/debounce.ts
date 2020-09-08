a3a5c779bf3069553f33c97d027806803ebb3f80
zpr1g <yuns.xie@qq.com>
2020-09-08 23:18:01 +0800

    refactor: rename variable, and add ref

--- docs/base/debounce/debounce.ts
+++ docs/base/debounce/debounce.ts
@@ -1,8 +1,10 @@
 export default function debounce(fn: Function, delay: number) {
-  let timeout: NodeJS.Timeout | undefined;
+  let timer: NodeJS.Timeout | undefined;
 
   return function(this: any) {
-    timeout && clearTimeout(timeout);
-    timeout = setTimeout(() => fn.apply(this, arguments), delay);
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, arguments), delay);
   };
 }
+
+// ref(ES2015 version): https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&spec=false&loose=false&code_lz=KYDwDg9gTgLgBAE2AMwIYFcA29noHYDGMAlhHosAEYT4HAAUyeAXHAGK0lkA0FmqAT1Z50AW0rAoASjgBvAFBw4mYPBKjJrAHIQkAKQDKAOgAqxDTXgAfOPiTJieYAgDc8xXCir0UcrkJcePQwABbEAM6sqHgCMgpKSuqScABkKXAEKqhQZhboMMHmklJuCXBJUHAAvHDhqrnAlvT0MlUAfHBMRqhgYJgCwWHhvNkA5mLAeDDhUrxI_LGlcAC-bsvyQA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Ces2015%2Ctypescript%2Cenv&prettier=false&targets=&version=7.11.6&externalPlugins=
