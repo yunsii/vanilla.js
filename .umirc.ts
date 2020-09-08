604ac6096eefd0ddf906d4a87a87511d31cbb6b3
zpr1g <yuns.xie@qq.com>
2020-09-08 22:17:51 +0800

    feat: exportStatic

--- .umirc.ts
+++ .umirc.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   logo:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
+  exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // more config: https://d.umijs.org/config
 });
