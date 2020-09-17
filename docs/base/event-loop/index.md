c61f2be83ad62fd78bfe56c2e2139e7c81d4ec70
zpr1g <yuns.xie@qq.com>
2020-09-17 23:35:15 +0800

    feat: event loop

---
+++ docs/base/event-loop/index.md
@@ -0,0 +1,5 @@
+# 事件循环
+
+[并发模型与事件循环 - MDN](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/EventLoop)
+
+<code src="./index.tsx" />
---
+++ docs/base/event-loop/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+function delay(wait: number = 1) {
+  return new Promise(res => {
+    setTimeout(() => {
+      res();
+    }, wait * 1000);
+  });
+}
+
+export default () => {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    delay().then(async () => {
+      while (true) {
+        await delay();
+        setCount(prevCount => {
+          console.log('prevCount', prevCount);
+          return prevCount + 1;
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => console.log('hello')}>console {count}</button>
+    </div>
+  );
+};
